d2573ceeaa087c026c74193558a1416ff8443210
Koike Takaaki <taka.vao.321@gmail.com>
2017-01-24 15:29:38 +0900

    ファイルアセットの追加

--- keijiban/src/jp/ac/oit/igakilab/dwr/keijiban/Keijiban.java
+++ keijiban/src/jp/ac/oit/igakilab/dwr/keijiban/Keijiban.java
@@ -40,4 +40,16 @@ public class Keijiban {
 		db.closeClient();
 		return true;
 	}
+
+    /**
+     * REST呼び出しを行う場合は下記のように呼ぶ
+     * http://sample.com:8080/project_name/dwr/jsonp/ClassName/MethodName/param1/ と指定する
+     * http://localhost:8080/multiple-dwr/dwr/jsonp/MultiplePrinter/helloWorld/ryokun/
+     * @param name
+     * @return nameに":HelloWorld"を付与したものを返す
+     */
+    public String helloWorld(String name){
+    	return name + ":HelloWorld";
+
+    }
 }
