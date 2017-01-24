fbf180d18429691dc95e848d90bb550eecdcdd67
koike-marshmallow <taka.vao.321@gmail.com>
2017-01-24 23:30:42 +0900

    ちょっと変更

--- ranking/WebContent/js/ranking.js
+++ ranking/WebContent/js/ranking.js
@@ -1,5 +1,11 @@
 var $rankList = $(".ranking"), $btn = $('.inpbtn'), $inpScore = $(".less");
 
+$(document).ready(function(){
+	$(".inpbtn").on('click', function(){
+		var name =
+	});
+});
+
 $btn.on('click',$inpScore,function(){
 
     var List = Ranking.getRanking("sinkei");
@@ -11,3 +17,19 @@ $btn.on('click',$inpScore,function(){
 
 
 });
+
+function showSinkeiRanking(){
+	Ranking.getRanking("sinkei", function(list){
+		var $rankList = $(".ranking");
+
+		$rankList.append("<table border='1' class='rlist'>" +
+			"<tr><th>順位</th><th>名前</th><th>点数</th></tr>" +
+			"</table>");
+
+		for(var tmp in list){
+			$(".rlist").append("<tr><td>" + tmp.rank +
+				"</td><td>" + tmp.name +
+				"</td><td>" + tmp.score + "</td></tr>");
+		}
+	});
+}
