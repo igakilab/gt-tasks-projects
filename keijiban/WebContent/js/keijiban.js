7ce51fa66dee5f59f5ff75743b3aa7d3b87d3665
Koike Takaaki <taka.vao.321@gmail.com>
2017-01-24 12:05:19 +0900

    連携完了

--- keijiban/WebContent/js/keijiban.js
+++ keijiban/WebContent/js/keijiban.js
@@ -1,29 +1,22 @@
 $board = $(".keijiban");
 $btn = $(".less")
-getMsg();
+reloadMessages();
 
 $btn.on('click', function (){
-	var name = $('#name').val();
-	var message = $('#message').val();
-	var days = new Date();
-	var month = days.getMonth()+1;
-	var time = days.getFullYear()+"/"+month+"/"+days.getDate()+" "+days.getHours()+":"+days.getMinutes()+":"+days.getSeconds();
-	//status = postMessage(name,message); 本番用
-	$board.append("<h4 class='name' style='display:inline'>"+name+"</h4>");
-	$board.append(" <h6 class='time' style='display:inline'>"+time+"</h6>");
-	$board.append("<p class='message'>"+message+"</p>");
-	$board.append("<hr>");
+	var toukou = $("#toukou").serializeJson();
+
+	Keijiban.postMessage(toukou, function(reply){
+		reloadMessages();
+	});
 });
 
-function getMsg(){
-	//var name = PostForm.name;
-	//var msg = PostForm.message;　本番用
-	//var time = PostForm.time;
-	var name="ああああああ"
-	var msg="メッセージメッセージメッセージメッセージメッセージ"
-	var time="1234/56/78 99:99:99"
-	$board.append("<h4 class='name' style='display:inline'>"+name+"</h4>");
-	$board.append(" <h6 class='time' style='display:inline'>"+time+"</h6>");
-	$board.append("<p class='message'>"+msg+"</p>");
-	$board.append("<hr>");
+function reloadMessages(){
+	Keijiban.getMessages(function(reply){
+		$board.empty();
+		for(var i=0; i<reply.length; i++){
+			$board.append("<h4 class='name' style='display:inline'>"+reply[i].name+"</h4>");
+			$board.append(" <h6 class='time' style='display:inline'>"+reply[i].time+"</h6>");
+			$board.append("<p class='message'>"+reply[i].message+"</p>");
+		}
+	});
 }
