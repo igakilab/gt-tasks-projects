4c19bc6cdb238f0596f7e88b28a851a3772003a5
Koike Takaaki <taka.vao.321@gmail.com>
2017-01-24 17:36:09 +0900

    keijibanを編集

--- keijiban/WebContent/js/script.js
+++ keijiban/WebContent/js/script.js
@@ -1,17 +1,19 @@
-$board = $(".keijiban");
-$btn = $(".less")
-reloadMessages();
+$(document).ready(function(){
+	reloadMessages();
 
-$btn.on('click', function (){
-	var toukou = $("#toukou").serializeJson();
+	$(".less").on('click', function(){
+		var toukou = $("#toukou").serializeJson();
 
-	Keijiban.postMessage(toukou, function(reply){
-		reloadMessages();
+		Keijiban.postMessage(toukou, function(reply){
+			reloadMessages();
+		});
 	});
 });
 
 function reloadMessages(){
 	Keijiban.getMessages(function(reply){
+		var $board = $(".keijiban");
+
 		$board.empty();
 		for(var i=0; i<reply.length; i++){
 			$board.append("<h4 class='name' style='display:inline'>"+reply[i].name+"</h4>");
