489a9dc2156112192bbfb099657696e2f6b4bb68
Koike Takaaki <taka.vao.321@gmail.com>
2017-01-26 23:31:27 +0900

    keijibanをマージ

--- keijiban/WebContent/js/script.js
+++ keijiban/WebContent/js/script.js
@@ -1,20 +1,34 @@
 $(document).ready(function(){
-	//reloadMessages();
-	loadThreads();
-	$(".less").on('click', function(){
-		var toukou = $("#toukou").serializeJson();
+	loadRooms();
 
-		Keijiban.postMessage(toukou, function(reply){
-			reloadMessages();
+	$(".less").on('click', function(){
+		var input = $("#toukou").serializeJson();
+
+		Keijiban.postMessage(input, function(reply){
+			loadMessages(input.room);
 		});
 	});
 });
 
-function reloadMessages(){
-	Keijiban.getMessages(function(reply){
+function initForm(room){
+	if( room ){
+		$(".room-header").css("display", "none");
+		$("#roomInput").val(room).attr("type", "hidden");
+		$(".less").text("投稿");
+	}else{
+		$(".room-header").css("display", "");
+		$("#roomInput").val("").attr("type", "text");
+		$(".less").text("ルーム作成");
+	}
+}
+
+function loadMessages(room){
+	Keijiban.getMessages(room, function(reply){
 		var $board = $(".keijiban");
+		initForm(room);
 
 		$board.empty();
+		$board.append("<h3>" + room + "</h3>");
 		for(var i=0; i<reply.length; i++){
 			$board.append("<h4 class='name' style='display:inline'>"+reply[i].name+"</h4>");
 			$board.append(" <h6 class='time' style='display:inline'>"+reply[i].time+"</h6>");
@@ -23,15 +37,19 @@ function reloadMessages(){
 	});
 }
 
-function loadThreads(){
-	$thread = $(".thread");
-	$thread.append("<h3>スレッド一覧</h3>");
-	$thread.append("<ul class='Tlist'><li><a href='#aaaaa'>aaaaa</a></li><li><a href='#bbbbb'>bbbbb</a></li></ul>");
-	$(".Tlist").on('click',function(){
-			$("#toukou").css("display","");
-			$(".keijiban").css("display","");
-			$thread.css("display","none");
-			//reloadMessages();
-	});
+function loadRooms(){
+	Keijiban.getRoomList(function(reply){
+		$list = $(".room-list");
+		initForm(null);
 
+		$list.empty();
+		for(var i=0; i<reply.length; i++){
+			var $link = $("<a></a>").attr("href", "#").text(reply[i]);
+			$link.on('click', {room:reply[i]}, function(e){
+				loadMessages(e.data.room);
+			});
+
+			$list.append($("<li></li>").append($link));
+		}
+	});
 }
